9af0690a36412f92f9630eec111edf35dcd2a3b9
Mal Greenidge <jamalgreenidge@gmail.com>
2021-08-20 23:48:47 -0400

    create array of messages

---
+++ message.js
@@ -0,0 +1,17 @@
+// Positive message generator That displays a random positive message when user runs program.....Real simple!!!
+
+const positiveMessage = [
+    "You are great!!",
+    "You are Confident!!",
+    "You are Successful!!",
+    "You are Smart!!",
+    "You are the greatest Software Engineer!!",
+    "You have nothing to worry about!!",
+    "You are fearless!!",
+    "Nothing can hurt you!!"
+];
+
+
+
+
+
